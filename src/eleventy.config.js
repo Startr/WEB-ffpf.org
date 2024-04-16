6edcc1c70a7daddf4f677866aa252389c2523781
OpenCo <admin@openco.ca>
2024-04-16 14:46:57 +0000

    Refactor Eleventy configuration files

--- src/eleventy.config.js
+++ src/eleventy.config.js
@@ -7,13 +7,9 @@ const pluginBundle = require("@11ty/eleventy-plugin-bundle");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
-const pluginDrafts = require("./.eleventy.config.drafts.js");
-const pluginImages = require("./.eleventy.config.images.js");
-
 module.exports = function(eleventyConfig) {
 	// Copy the contents of the `public` folder to wp-content to emulate WordPress behavior. 
 	eleventyConfig.addPassthroughCopy({
 		"./public/": "/wp-content/"
 	});
-
 };
